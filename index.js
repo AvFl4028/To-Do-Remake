4eeee3319ae910bef5d3697e09ed67a8611b7ce2
AvFl <82409897+AvielFl@users.noreply.github.com>
2021-07-22 21:41:03 -0500

    update

---
+++ index.js
@@ -0,0 +1,54 @@
+(function(){
+	// Variables
+	var lista = document.getElementById("lista"),
+		tareaInput = document.getElementById("tareaInput"),
+		btnNuevaTarea = document.getElementById("btn-agregar");
+
+	// Funciones
+	var agregarTarea = function(){
+		var tarea = tareaInput.value,
+			nuevaTarea = document.createElement("li"),
+			enlace = document.createElement("a"),
+			contenido = document.createTextNode(tarea);
+
+		if (tarea === "") {
+			tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
+			tareaInput.className = "error";
+			return false;
+		}else {
+			tareaInput.setAttribute('placeholder','Agrega tu tarea');
+			tareaInput.className = 'type';
+		}
+
+		// Agregamos el contenido al enlace
+		enlace.appendChild(contenido);
+		// Le establecemos un atributo href
+		enlace.setAttribute("href", "#");
+		// Agrergamos el enlace (a) a la nueva tarea (li)
+		nuevaTarea.appendChild(enlace);
+		// Agregamos la nueva tarea a la lista
+		lista.appendChild(nuevaTarea);
+
+		tareaInput.value = "";
+
+		for (var i = 0; i <= lista.children.length -1; i++) {
+			lista.children[i].addEventListener("click", function(){
+				this.parentNode.removeChild(this);
+			});
+		}
+
+	};
+
+	var eleminarTarea = function(){
+		this.parentNode.removeChild(this);
+	};
+
+	// Agregar Tarea
+	btnNuevaTarea.addEventListener("click", agregarTarea);
+
+
+	// Borrando Elementos de la lista
+	for (var i = 0; i <= lista.children.length -1; i++) {
+		lista.children[i].addEventListener("click", eleminarTarea);
+	}
+}());
